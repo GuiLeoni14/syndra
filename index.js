3e6308c1ce9f3e48877062dbee7e240d77b47dcb
GuiLeoni14 <guileone167@gmail.com>
2023-04-02 20:16:04 -0300

    chore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
 
@@ -13,19 +13,65 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildIntegrations,
     GatewayIntentBits.GuildMembers,
   ],
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  // Definir os comandos para o bot
+  const commands = [
+    {
+      name: 'ping',
+      description: 'Pong!',
+    },
+    {
+      name: 'chat',
+      description: 'Diga algo',
+      options: [
+        {
+          name: 'question',
+          description: 'A pergunta que você quer fazer',
+          type: 3,
+          required: true,
+        },
+      ],
+    },
+  ];
+
+  // Registrar os comandos com o Discord
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_SECRET);
+
+  try {
+    await rest.put(Routes.applicationCommands(client.user.id), { body: commands });
+
+    console.log('Successfully registered application commands.');
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isCommand() || interaction.commandName !== 'chat') return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  await interaction.reply('Um segundo docinho estou verificando...');
+  try {
+    const question = interaction.options.getString('question');
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: question,
+      max_tokens: 500,
+      n: 1,
+    });
+
+    const answer = response.data.choices[0].text.trim();
+    console.log(answer);
+    await interaction.followUp(answer.toString() || 'FUDEO');
+  } catch (error) {
+    console.error('Ocorreu um erro:', JSON.stringify(error));
+    await interaction.followUp('Vixi docinho, tive alguns probleminhas e não consegui encontrar...');
   }
 });
 
@@ -33,6 +79,11 @@ client.on('messageCreate', async (msg) => {
   if (msg.author.username.toLowerCase() !== 'syndra') {
     if (msg.content.includes('chat')) {
       try {
+        console.log(msg.content);
+        if (msg.content.toLowerCase() === 'chat qual seu nome?') {
+          await msg.reply('Meu nome é Syndra!');
+          return;
+        }
         const response = await openai.createCompletion({
           model: 'text-davinci-003',
           prompt: msg.content,
@@ -48,4 +99,5 @@ client.on('messageCreate', async (msg) => {
     }
   }
 });
+
 client.login(process.env.DISCORD_SECRET);
